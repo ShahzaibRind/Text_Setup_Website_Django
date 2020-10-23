6c34eac9c5b89970b4aebc0be578cb92597e97b9
Shahzaib Rind <shahzaibrind86@gmail.com>
2020-10-23 22:44:48 +0500

    first commit

---
+++ textutlis/views.py
@@ -0,0 +1,58 @@
+#This file is created from me
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html',)
+
+
+def analyze(request):
+    # Get the Text DJango Text
+    djtext = request.POST.get('text', 'default')
+
+
+    #Check the Checkbox Values
+    removepunc = request.POST.get('removepunc', 'off')
+    fullcaps = request.POST.get('fullcaps', 'off')
+    newlineremover = request.POST.get('newlineremover', 'off')
+    extraspaceremover = request.POST.get('extraspaceremover', 'off')
+    # charcounter = request.POST.get('charcounter', 'off')
+
+    #Check the which Checkbox is on
+    if removepunc == 'on':
+        punctuations = '''!()-[]{};:'"\,<>./@#$%^&*~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+        params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
+        djtext = analyzed
+        # analyze the Text
+        # return render(request, 'Analyze.html', params)
+    if(fullcaps == "on"):
+        analyzed = " "
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+        params = {'purpose' :'change to Uppercase', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+    if (extraspaceremover == "on"):
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not (djtext[index] == " " and djtext[index+1] == " "):
+                analyzed = analyzed + char
+        params = {'purpose': 'Extra Space Removed', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+    if(newlineremover == "on"):
+        analyzed = ""
+        for char in djtext:
+            if char != "\n" and char != "\r":
+                analyzed = analyzed + char
+        params = {'purpose':'New Line Remover', 'analyzed_text':analyzed}
+
+    if(removepunc != "on" and fullcaps != "on" and newlineremover != "on" and extraspaceremover != "on"):
+        return HttpResponse("ERROR! Please select at least one Option")
+    return render(request, 'Analyze.html', params)
